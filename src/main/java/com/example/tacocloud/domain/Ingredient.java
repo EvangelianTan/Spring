b61cfd31d64bf875582e7e9b667936fbb3120653
EvangelianTan <1223103569@qq.com>
2021-11-17 14:29:57 +0800

    第一章实现控制类 建立Spring框架

---
+++ src/main/java/com/example/tacocloud/domain/Ingredient.java
@@ -0,0 +1,33 @@
+package com.example.tacocloud.domain;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import lomback.Data;
+public class Ingredient {
+    private final String id;
+    public String getId() {
+        return id;
+    }
+
+    private final String name;
+    public String getName() {
+        return name;
+    }
+
+    private final Type type;
+    public Type getType() {
+        return type;
+    }
+
+
+    public static enum Type{
+        WRAP,PROTEIN,VEGGIES,CHEESE,SAUCE
+    }
+
+
+    public Ingredient(String id, String name, Type type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+}
